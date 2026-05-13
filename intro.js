1503c13e90b4cdd6da25adbcb7916ba322ad5a59
Alakeji Elizabeth <elizabethalakeji@gmail.com>
2026-05-13 16:23:20 +0100

    Added JavaScript console log code

---
+++ intro.js
@@ -0,0 +1,7 @@
+let name = "Elizabeth";
+let age = 20;
+let favoriteProgrammingLanguage = "JavaScript";
+
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Favorite Programming Language:", favoriteProgrammingLanguage);
